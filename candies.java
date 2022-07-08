231494eb6d0f2e30735331b0a68feb20f58023a5
yatin987 <67496077+yatin987@users.noreply.github.com>
2022-07-08 12:06:22 +0530

    Create candies.java

---
+++ candies.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = -1;
+        for(int i=0; i<candies.length; i++) {
+            if(candies[i] > max) max = candies[i];
+        }
+        List<Boolean> ans = new ArrayList<>();
+        for(int i=0; i<candies.length; i++) {
+            if(candies[i] + extraCandies >= max) {
+                ans.add(true);
+            } else ans.add(false);
+        }
+        return ans;
+    }
+}
